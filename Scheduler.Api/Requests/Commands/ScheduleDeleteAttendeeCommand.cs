2101f1b3eebdd3fd9510d19a3fd27950f497995f
Chris Russell <48478211+chrisrivendi@users.noreply.github.com>
2020-02-04 08:10:34 +0000

    Delete attendee from schedule

---
+++ Scheduler.Api/Requests/Commands/ScheduleDeleteAttendeeCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Scheduler.Api.Requests.Commands
+{
+    public class ScheduleDeleteAttendeeCommand : IRequest
+    {
+        public int UserId { get; set; }
+        public int ScheduleId { get; set; }
+    }
+}
---
+++ Scheduler.Api/Requests/Handlers/ScheduleDeleteAttendeeCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Scheduler.Api.Requests.Commands;
+using Scheduler.Data.Abstract;
+
+namespace Scheduler.Api.Requests.Handlers
+{
+    public class ScheduleDeleteAttendeeCommandHandler : IRequestHandler<ScheduleDeleteAttendeeCommand>
+    {
+        private IAttendeeRepository _attendeeRepository;
+        private IMapper _mapper;
+
+        public ScheduleDeleteAttendeeCommandHandler(IAttendeeRepository attendeeRepository, IMapper mapper)
+        {
+            _attendeeRepository = attendeeRepository;
+            _mapper = mapper;
+        }
+
+        public Task<Unit> Handle(ScheduleDeleteAttendeeCommand request, CancellationToken cancellationToken)
+        {
+            if (_attendeeRepository.FindBy(a => a.UserId == request.UserId && 
+                                                                a.ScheduleId == request.ScheduleId) == null)
+            {
+                throw new KeyNotFoundException($"User {request.UserId} is not associated with schedule {request.ScheduleId}");
+            }
+            _attendeeRepository.DeleteWhere(a => a.UserId == request.UserId && a.ScheduleId == request.ScheduleId);
+            _attendeeRepository.Commit();
+
+            return Unit.Task;
+        }
+    }
+}
