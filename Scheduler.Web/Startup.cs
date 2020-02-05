2d523eb241990057d9b5bbc7b87bc6d66521ec73
Chris Russell <48478211+chrisrivendi@users.noreply.github.com>
2020-02-05 21:50:30 +0000

    Basic schedule api query in Blazor

--- Scheduler.Web/Startup.cs
+++ Scheduler.Web/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
@@ -28,7 +29,7 @@ namespace Scheduler.Web
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<HttpClient>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
